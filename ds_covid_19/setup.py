e2b59695d992fbf475eb73a09f4283f12c9700a9
Anuj Maharjan <maharjan@rhrk.uni-kl.de>
2020-06-11 13:02:17 +0200

    cookiecutter setup

---
+++ ds_covid_19/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='applied data science on COVID19 data',
+    author='anuj',
+    license='',
+)
